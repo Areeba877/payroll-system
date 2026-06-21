e9d73fd4d06a14046900b29d5acd208ba05156bd
Areeba Khalid <areebakhalid877@gmail.com>
2026-06-21 11:37:30 -0700

    Payroll System

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
-from flask import Flask, render_template, request
+
+from flask import Flask, render_template, request, redirect, session, send_file
 import sqlite3
+from reportlab.pdfgen import canvas
 
 app = Flask(__name__)
+app.secret_key = "payroll_secret_key"
 
 # ==========================
 # Database Connection
@@ -28,10 +31,33 @@ def home():
 def login():
 
     if request.method == "POST":
+
         username = request.form["username"]
-        return f"Welcome {username}"
+
+        session["username"] = username
+        session["role"] = "Admin"
+
+        return redirect("/dashboard")
 
     return render_template("login.html")
+
+@app.route("/dashboard")
+def dashboard():
+
+    if "username" not in session:
+        return redirect("/login")
+
+    return render_template(
+        "dashboard.html",
+        username=session["username"],
+        role=session["role"]
+    )
+
+
+@app.route("/logout")
+def logout():
+    session.clear()
+    return redirect("/")
 
 
 # ==========================
@@ -80,16 +106,99 @@ def add_employee():
 
 
 # ==========================
+# Delete Employee
+# ==========================
+
+@app.route("/delete_employee/<int:id>")
+def delete_employee(id):
+
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    cursor.execute(
+        "DELETE FROM Employees WHERE employee_id = ?",
+        (id,)
+    )
+
+    conn.commit()
+    conn.close()
+
+    return redirect("/employees")
+
+
+# ==========================
+# Update Employee
+# ==========================
+
+@app.route("/update_employee/<int:id>", methods=["GET", "POST"])
+def update_employee(id):
+
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    if request.method == "POST":
+
+        cursor.execute("""
+        UPDATE Employees
+        SET full_name=?,
+            email=?,
+            phone=?,
+            department=?,
+            designation=?
+        WHERE employee_id=?
+        """, (
+
+            request.form["full_name"],
+            request.form["email"],
+            request.form["phone"],
+            request.form["department"],
+            request.form["designation"],
+            id
+        ))
+
+        conn.commit()
+        conn.close()
+
+        return redirect("/employees")
+
+    cursor.execute(
+        "SELECT * FROM Employees WHERE employee_id=?",
+        (id,)
+    )
+
+    employee = cursor.fetchone()
+
+    conn.close()
+
+    return render_template(
+        "update_employee.html",
+        employee=employee
+    )
+
+
+# ==========================
 # Employee List
 # ==========================
 
 @app.route("/employees")
 def employee_list():
 
+    search = request.args.get("search", "")
+
     conn = get_connection()
     cursor = conn.cursor()
 
-    cursor.execute("SELECT * FROM Employees")
+    cursor.execute("""
+        SELECT * FROM Employees
+        WHERE full_name LIKE ?
+        OR department LIKE ?
+        OR employee_id LIKE ?
+    """, (
+        f"%{search}%",
+        f"%{search}%",
+        f"%{search}%"
+    ))
+
     employees = cursor.fetchall()
 
     conn.close()
@@ -143,6 +252,70 @@ def department_list():
     return render_template(
         "department_list.html",
         departments=departments
+    )
+
+# ==========================
+# Delete Department
+# ==========================
+
+@app.route("/delete_department/<int:id>")
+def delete_department(id):
+
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    cursor.execute(
+        "DELETE FROM Departments WHERE department_id=?",
+        (id,)
+    )
+
+    conn.commit()
+    conn.close()
+
+    return redirect("/departments")
+
+
+# ==========================
+# Update Department
+# ==========================
+
+@app.route("/update_department/<int:id>", methods=["GET", "POST"])
+def update_department(id):
+
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    if request.method == "POST":
+
+        cursor.execute(
+            """
+            UPDATE Departments
+            SET department_name=?
+            WHERE department_id=?
+            """,
+            (
+                request.form["department_name"],
+                id
+            )
+        )
+
+        conn.commit()
+        conn.close()
+
+        return redirect("/departments")
+
+    cursor.execute(
+        "SELECT * FROM Departments WHERE department_id=?",
+        (id,)
+    )
+
+    department = cursor.fetchone()
+
+    conn.close()
+
+    return render_template(
+        "update_department.html",
+        department=department
     )
 
 
@@ -331,6 +504,7 @@ def generate_payroll():
 @app.route("/payslip/<employee_id>")
 def payslip(employee_id):
 
+
     conn = get_connection()
     cursor = conn.cursor()
 
@@ -378,6 +552,18 @@ def payslip(employee_id):
         net_salary=net_salary
     )
 
+@app.route("/payslip/<employee_id>/download")
+def download_payslip(employee_id):
+
+    file_name = f"payslip_{employee_id}.pdf"
+
+    c = canvas.Canvas(file_name)
+    c.drawString(100, 800, "PAYSLIP")
+    c.drawString(100, 770, f"Employee ID: {employee_id}")
+    c.save()
+
+    return send_file(file_name, as_attachment=True)
+
 @app.route("/payroll_report")
 def payroll_report():
 
@@ -385,12 +571,65 @@ def payroll_report():
     cursor = conn.cursor()
 
     cursor.execute("""
-    SELECT employee_id,
-           basic_salary,
-           house_allowance,
-           medical_allowance,
-           transport_allowance
+    SELECT
+        employee_id,
+        basic_salary,
+        house_allowance,
+        medical_allowance,
+        transport_allowance,
+        tax,
+        loan_deduction,
+        other_deduction
     FROM SalaryStructure
+    """)
+
+    records = cursor.fetchall()
+    conn.close()
+
+    report_data = []
+
+    for row in records:
+
+        gross_salary = (
+            float(row[1]) +
+            float(row[2]) +
+            float(row[3]) +
+            float(row[4])
+        )
+
+        deductions = (
+            float(row[5]) +
+            float(row[6]) +
+            float(row[7])
+        )
+
+        net_salary = gross_salary - deductions
+
+        report_data.append([
+            row[0],          # Employee ID
+            row[1],          # Basic Salary
+            gross_salary,
+            deductions,
+            net_salary
+        ])
+
+    return render_template(
+        "payroll_report.html",
+        records=report_data
+    )
+
+@app.route("/department_salary_report")
+def department_salary_report():
+
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    cursor.execute("""
+    SELECT
+        e.department,
+        COUNT(*)
+    FROM Employees e
+    GROUP BY e.department
     """)
 
     records = cursor.fetchall()
@@ -398,7 +637,82 @@ def payroll_report():
     conn.close()
 
     return render_template(
-        "payroll_report.html",
+        "department_salary_report.html",
+        records=records
+    )
+
+@app.route("/employee_payroll_history")
+def employee_payroll_history():
+
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    cursor.execute("""
+    SELECT
+        employee_id,
+        basic_salary,
+        house_allowance,
+        medical_allowance,
+        transport_allowance,
+        tax,
+        loan_deduction,
+        other_deduction
+    FROM SalaryStructure
+    """)
+
+    records = cursor.fetchall()
+    conn.close()
+
+    history = []
+
+    for row in records:
+
+        gross_salary = (
+            float(row[1]) +
+            float(row[2]) +
+            float(row[3]) +
+            float(row[4])
+        )
+
+        deductions = (
+            float(row[5]) +
+            float(row[6]) +
+            float(row[7])
+        )
+
+        net_salary = gross_salary - deductions
+
+        history.append([
+            row[0],
+            gross_salary,
+            deductions,
+            net_salary
+        ])
+
+    return render_template(
+        "employee_payroll_history.html",
+        history=history
+    ) 
+
+@app.route("/attendance_report")
+def attendance_report():
+
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    cursor.execute("""
+    SELECT
+        employee_id,
+        status
+    FROM Attendance
+    """)
+
+    records = cursor.fetchall()
+
+    conn.close()
+
+    return render_template(
+        "attendance_report.html",
         records=records
     )
 
@@ -417,7 +731,6 @@ def profile():
         "profile.html",
         employee=employee
     )
-
 
 if __name__ == "__main__":
     app.run(debug=True)
